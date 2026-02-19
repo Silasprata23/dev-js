6d2dbbedd2c8594d94a74846e4e37c1bf0211fcb
Silasprata23 <silasprata23@gmail.com>
2026-02-19 19:35:38 -0300

    feat:exercise two

---
+++ aulas/two/tipo.js
@@ -0,0 +1,21 @@
+let nome = "cinco";
+let numero = 5;
+let validade = 1;
+
+console.log(typeof nome);
+console.log(typeof numero);
+console.log(typeof validade);
+
+nome = 5;
+numero ="dez";
+validade = true
+
+console.log(typeof nome);
+console.log(typeof numero);
+console.log(typeof validade);
+
+// Na variável nome era string e foi trasformada em number
+// Na variável numero era number e foi trasformada em string 
+// Na variável validade era number e foi trasformada em boolean 
+
+
