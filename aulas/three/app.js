70eb58ed6187a90f41c8a9a7ebc590eb9a4e3773
Silasprata23 <silasprata23@gmail.com>
2026-02-23 18:54:49 -0300

    feat:exercise Three

---
+++ aulas/three/app.js
@@ -0,0 +1,10 @@
+let idade = 19;
+const nome = "Silas";
+let cidade = "São paulo";
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos e moro em ${cidade}`);
+
+let num1 = 15;
+let num2 = 95;
+let soma = num1 + num2;
+console.log(`o resultado da soma é ${soma}` );
